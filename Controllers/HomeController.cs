a67383363d66ef3da7da21ca0a6b616cd2112b94
blacksilla <afonsosilva1@ua.pt>
2023-03-02 19:27:05 +0000

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using LoginForm.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LoginForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Validation(string email, string password)
+        {
+            if(email != null || password != null)
+            {
+                //email:123@123.com - password 123
+                if (email == "123@123.com" && password == "123")
+                {
+                    return View("Main");
+                }
+                else
+                {
+                    return View(" ");
+                }
+            }
+            else
+            {
+                return View(" ");
+            }
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
